57345b5b76889f3b2e9adfe6f35ec0bc5c26f804
Damian Barsotti <damian@famaf.unc.edu.ar>
2021-10-27 15:59:09 -0300

    Initial

---
+++ config.h
@@ -0,0 +1,88 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wifi: SSID and password
+const PROGMEM char* WIFI_SSID = "[Redacted]";
+const PROGMEM char* WIFI_PASSWORD = "[Redacted]";
+
+// If 0.0.0.0 use dhcp
+//const IPAddress local_IP(0,0,0,0);
+
+// If neded set your Static IP address
+const IPAddress local_IP(192, 168, 0, 53);
+// Set your Gateway IP address
+const IPAddress gateway(192, 168, 0, 1);
+const IPAddress subnet(255, 255, 255, 0);
+// Optional DNS
+//IPAddress primaryDNS(8, 8, 8, 8);   //optional
+//IPAddress secondaryDNS(8, 8, 4, 4); //optional
+
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char* MQTT_CLIENT_ID = "home_sensor1_dht11";
+const PROGMEM char* MQTT_SERVER_IP = "192.168.0.5";
+const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
+const PROGMEM char* MQTT_USER = "[Redacted]";
+const PROGMEM char* MQTT_PASSWORD = "[Redacted]";
+
+// MQTT: topics
+const PROGMEM char* MQTT_SENSOR_TOPIC = "home/living/ac/sensor";
+const PROGMEM char* MQTT_LOG_TOPIC = "home/living/ac/log";
+const PROGMEM char* MQTT_AC_GET_TOPIC = "home/living/ac/get";
+
+const PROGMEM char* MQTT_AC_POWER_SET_TOPIC = "home/living/ac/power/set";
+const PROGMEM char* MQTT_AC_MODE_SET_TOPIC = "home/living/ac/mode/set";
+const PROGMEM char* MQTT_AC_TEMP_SET_TOPIC = "home/living/ac/temp/set";
+const PROGMEM char* MQTT_AC_FAN_SET_TOPIC = "home/living/ac/fan/set";
+const PROGMEM char* MQTT_AC_SWING_SET_TOPIC = "home/living/ac/swing/set";
+
+
+const PROGMEM char* COMM_ON = "on";
+const PROGMEM char* COMM_OFF = "off";
+const PROGMEM char* COMM_AUTO = "auto";
+const PROGMEM char* COMM_HEAT = "heat";
+const PROGMEM char* COMM_COOL = "cool";
+const PROGMEM char* COMM_DRY = "dry";
+const PROGMEM char* COMM_FAN_ONLY = "fan_only";
+
+const PROGMEM char* COMM_FAN_AUTO = "auto";
+const PROGMEM char* COMM_FAN_LOW = "low";
+const PROGMEM char* COMM_FAN_MED = "medium";
+const PROGMEM char* COMM_FAN_HIGH = "high";
+const PROGMEM char* COMM_FAN_TURBO = "turbo";
+
+// sleeping time
+const uint16_t SLEEPING_TIME_IN_MSECONDS = 500; 
+
+// read sensor interval time
+const uint16_t READ_SENSOR_TIME_IN_SECONDS = 20; 
+
+// send data configuratio connection to mqtt
+const bool LOG_MQTT_CONNECT = false;
+
+
+// To adjust reads of DHT11
+/*
+const float temp_slope = 1.0f;
+const float temp_shift = 0.0f;
+const float humid_slope = 1.0f;
+const float humid_shift = 0.0f;
+*/
+const float temp_slope = 1.029f;
+const float temp_shift = -5.01f;
+const float humid_slope = 0.899f;
+const float humid_shift = 21.285f;
+
+#ifdef  ARDUINO_ESP8266_GENERIC
+#define D2 4
+#define D3 0
+#define D4 2
+#define D9 3
+#endif
+
+const uint8_t DHTPIN = D4;
+const uint8_t DHTTYPE = DHT11;
+
+const uint16_t kIrLed = D2;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+//const uint16_t kIrLed = D9;
+
+#endif
---
+++ ha_mqtt_ir_dht11.ino
@@ -0,0 +1,498 @@
+/*
+ *   MQTT Sensor - Temperature and Humidity (DHT22) for Home-Assistant - NodeMCU (ESP8266)
+ *   https://home-assistant.io/components/sensor.mqtt/
+ * 
+ *   Libraries :
+ *    - ESP8266 core for Arduino : https://github.com/esp8266/Arduino
+ *    - PubSubClient : https://github.com/knolleary/pubsubclient
+ *    - DHT : https://github.com/adafruit/DHT-sensor-library
+ *    - ArduinoJson : https://github.com/bblanchon/ArduinoJson
+ * 
+ *   Sources :
+ *    - File > Examples > ES8266WiFi > WiFiClient
+ *    - File > Examples > PubSubClient > mqtt_auth
+ *    - File > Examples > PubSubClient > mqtt_esp8266
+ *    - File > Examples > DHT sensor library > DHTtester
+ *    - File > Examples > ArduinoJson > JsonGeneratorExample
+ *    - http://www.jerome-bernard.com/blog/2015/10/04/wifi-temperature-sensor-with-nodemcu-esp8266/
+ * 
+ *   Schematic :
+ *    - https://github.com/mertenats/open-home-automation/blob/master/ha_mqtt_sensor_dht22/Schematic.png
+ *    - DHT22 leg 1 - VCC
+ *    - DHT22 leg 2 - D1/GPIO5 - Resistor 4.7K Ohms - GND
+ *    - DHT22 leg 4 - GND
+ *    - D0/GPIO16 - RST (wake-up purpose)
+ * 
+ *   Configuration (HA) :
+sensor 1:
+    platform: mqtt
+    name: "Temperature ESP8266 1"
+    state_topic: "home/esp8266_1/sensor"
+    unit_of_measurement: "°C"
+    device_class: "temperature"
+    value_template: "{{ value_json.temperature }}"
+
+sensor 2:
+    platform: mqtt
+    name: "Humidity ESP8266 1"
+    state_topic: "home/esp8266_1/sensor"
+    unit_of_measurement: "%"
+    device_class: "humidity"
+    value_template: "{{ value_json.humidity }}"
+
+sensor 3:
+    platform: mqtt
+    name: "Logger ESP8266 1"
+    state_topic: "home/esp8266_1/log"#    
+    value_template: "{{ value_json.log }}"
+
+Damian Barsotti - v1.0 02.2021
+Based on:
+ *   Samuel M. - v1.1 - 08.2016
+ *   https://github.com/mertenats/open-home-automation
+ * 
+ */
+
+#include <ESP8266WiFi.h>
+
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+#include <PubSubClient.h>
+
+#include <ArduinoJson.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <ir_Samsung.h>
+#include <DHT.h>
+
+#include "config_local.h" // File for testing outside git
+#include "config.h"
+
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+IRSamsungAc ac(kIrLed);     // Set the GPIO used for sending messages.
+
+void publish(DynamicJsonDocument root, const char* topic){
+    
+    String data;
+    
+    serializeJson(root, data);
+    
+    Serial.print("Publish in topic "); Serial.println(topic);
+    
+    int res = client.publish(topic, data.c_str(), true);
+    
+    serializeJson(root, Serial);
+    Serial.println();
+
+    // if (res != 1)
+    //     Serial.println(String("Publish result: ") + String(res));  
+}
+
+String header_log(char* level, int n_log){
+    return String(level) + " " + String(n_log) + ": ";
+}
+
+String header_log_info(int n_log){
+    return header_log("INFO", n_log);
+}
+
+String header_log_warn(int n_log){
+    return header_log("WARN", n_log);
+}
+
+// function to log throught Serial and mqtt topic
+bool logger(String msg){
+
+    Serial.println(msg);
+    if (client.connected()) {
+        DynamicJsonDocument root(msg.length()+17);
+        root["log"] = msg;
+        publish(root, MQTT_LOG_TOPIC);
+        return true;
+    } else {
+        Serial.print("ERROR: failed MQTT connection in logger, rc=");
+        Serial.println(client.state());
+        return false;
+    }
+}
+
+int n_log_info = 0;
+int n_log_warn = 0;
+
+void logger_info(String msg){
+    if (logger(header_log_info(n_log_info) + msg))
+        n_log_info++;
+}
+
+void logger_warn(String msg){
+    if (logger(header_log_warn(n_log_warn) + msg))
+        n_log_warn++;
+}
+
+// function called to publish the temperature and the humidity
+void publish_data_sensor(float p_temperature, float p_humidity) {
+    DynamicJsonDocument root(200);
+    root["temperature"] = (String)p_temperature;
+    root["humidity"] = (String)p_humidity;
+    
+    publish(root, MQTT_SENSOR_TOPIC);
+}
+
+// function called to publish the AC state
+void publish_ac_state() {
+    DynamicJsonDocument root(200);
+
+    root["temperature"] = String(ac.getTemp());
+    switch (ac.getFan())
+    {
+    case kSamsungAcFanAuto:
+        root["fan"] = String("auto");
+        break;
+    
+    case kSamsungAcFanLow:
+        root["fan"] = String("low");
+        break;
+    
+    case kSamsungAcFanMed:
+        root["fan"] = String("medium");
+        break;
+    
+    case kSamsungAcFanHigh:
+        root["fan"] = String("high");
+        break;
+    
+    case kSamsungAcFanTurbo:
+        root["fan"] = String("turbo");
+        break;
+    
+    default:
+        break;
+    }
+    
+    if (!ac.getPower())
+        root["mode"] = String("off");
+    else
+        switch (ac.getMode())
+        {
+        case kSamsungAcAuto:
+            root["mode"] = String("auto");
+            break;
+
+        case kSamsungAcCool:
+            root["mode"] = String("cool");
+            break;
+
+        case kSamsungAcDry:
+            root["mode"] = String("dry");
+            break;
+
+        case kSamsungAcFan:
+            root["mode"] = String("fan");
+            break;
+
+        case kSamsungAcHeat:
+            root["mode"] = String("heat");
+            break;
+
+        default:
+            break;
+        }
+
+    publish(root, MQTT_AC_GET_TOPIC);
+}
+
+void print_ac_state() {
+  logger_info(ac.toString());
+}
+
+void ac_turn_on() {
+    ac.on();
+    ac.sendExtended();
+    ac.send();
+    print_ac_state();
+}
+
+void ac_turn_off() {
+    ac.off();
+    ac.sendExtended();
+    ac.send();
+    print_ac_state();
+}
+
+// function called when a MQTT message arrived
+void callback(char* topic, byte* payload, unsigned int length) {
+    char command[length+1];
+    
+    if (strcmp(topic, MQTT_AC_GET_TOPIC) == 0)
+        return;
+
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    
+    for (int i = 0; i < length; i++)
+        command[i] = tolower((char)payload[i]);
+    command[length] = '\0';
+    
+    Serial.println(command);
+
+    if (strcmp(MQTT_AC_POWER_SET_TOPIC, topic) == 0) {
+      
+        if (strcmp(COMM_OFF, command) == 0) {
+            Serial.println("Turn off the A/C ...");
+            ac_turn_off();
+        } else { //if (strcmp(COMM_ON, command) == 0) {
+            Serial.println("Turn on the A/C ...");
+            ac_turn_on();
+        }
+        print_ac_state();
+        delay(1000);
+
+    } else
+    if (strcmp(MQTT_AC_MODE_SET_TOPIC, topic) == 0) {
+    
+        if (!ac.getPower() && strcmp(COMM_OFF, command) != 0) {
+            Serial.println("Turn on the A/C ...");
+            ac.on();
+        }
+        if (strcmp(COMM_AUTO, command) == 0) {
+            Serial.println("Set the A/C mode to auto ...");
+            ac.setMode(kSamsungAcAuto);
+        } else 
+        if (strcmp(COMM_HEAT, command) == 0) {
+            Serial.println("Set the A/C mode to heat ...");
+            ac.setMode(kSamsungAcHeat);
+        } else 
+        if (strcmp(COMM_COOL, command) == 0) {
+            Serial.println("Set the A/C mode to cooling ...");
+            ac.setMode(kSamsungAcCool);
+        } else 
+        if (strcmp(COMM_DRY, command) == 0) {
+            Serial.println("Set the A/C mode to dry ...");
+            ac.setMode(kSamsungAcDry);
+        } else 
+        if (strcmp(COMM_FAN_ONLY, command) == 0) {
+            Serial.println("Set the A/C mode to fan ...");
+            ac.setMode(kSamsungAcFan);
+        } else 
+          return;
+
+    } else
+    if (strcmp(MQTT_AC_TEMP_SET_TOPIC, topic) == 0) {
+      
+      int temp = round(String(command).toFloat());
+      Serial.print("Set temperature to "); Serial.println(temp);
+      ac.setTemp(temp);      
+
+    } else
+    if (strcmp(MQTT_AC_FAN_SET_TOPIC, topic) == 0) {
+
+        if (strcmp(COMM_FAN_AUTO, command) == 0) {
+            Serial.println("Set the A/C fan to auto ...");
+            ac.setFan(kSamsungAcFanAuto);
+        } else 
+        if (strcmp(COMM_FAN_LOW, command) == 0) {
+            Serial.println("Set the A/C fan to low ...");
+            ac.setFan(kSamsungAcFanLow);
+        } else 
+        if (strcmp(COMM_FAN_MED, command) == 0) {
+            Serial.println("Set the A/C fan to medium ...");
+            ac.setFan(kSamsungAcFanMed);
+        } else 
+        if (strcmp(COMM_FAN_HIGH, command) == 0) {
+            Serial.println("Set the A/C fan to high ...");
+            ac.setFan(kSamsungAcFanHigh);
+        } else 
+        if (strcmp(COMM_FAN_TURBO, command) == 0) {
+            Serial.println("Set the A/C fan to turbo ...");
+            ac.setFan(kSamsungAcFanTurbo);
+        } else 
+          return;
+
+    } else
+        return;
+    
+    publish_ac_state();
+    print_ac_state();
+
+    if (!ac.getPower()) {
+        Serial.println("AC turned off not sending.");
+        return;
+    } else
+        ac.send();
+
+    delay(500);
+}
+
+void send_mqtt_connect_info(int attempt){
+
+    if (LOG_MQTT_CONNECT){
+      logger_info(String("IP address: ") + WiFi.localIP().toString());
+      logger_info(String("MQTT SERVER IP: ") + MQTT_SERVER_IP + ":" + MQTT_SERVER_PORT);
+      logger_info(String("MQTT CLIENT ID: ") + MQTT_CLIENT_ID);
+      logger_info(String("temp slope: ") + temp_slope + String(", temp shift: ") + temp_shift
+        + String(", humid slope: ") + humid_slope + String(", humid shift: ") + humid_shift);
+      logger_info(String("Number of attempts: ") + String(attempt));
+    }
+}
+
+bool mqtt_connect() {
+    // Loop until we're reconnected
+    static const int max_attempt = 10;
+    int attempt = 0;
+    
+    while (attempt < max_attempt && !client.connected()) {
+        Serial.println("INFO: Attempting MQTT connection...");
+        // Attempt to connect
+        if (! client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD)) {
+            Serial.print("ERROR: failed, rc="); Serial.println(client.state());
+            Serial.println("DEBUG: try again in 2 seconds");
+            // Wait 2 seconds before retrying
+            delay(2000);
+        }
+        attempt++;
+    }
+
+    if (attempt < max_attempt){
+        send_mqtt_connect_info(attempt);
+        client.subscribe(MQTT_AC_POWER_SET_TOPIC);
+        client.subscribe(MQTT_AC_MODE_SET_TOPIC);
+        client.subscribe(MQTT_AC_TEMP_SET_TOPIC);
+        client.subscribe(MQTT_AC_FAN_SET_TOPIC);
+        client.subscribe(MQTT_AC_SWING_SET_TOPIC);
+        client.subscribe(MQTT_AC_GET_TOPIC);
+        logger_info(String("MQTT connect in ") + attempt + " attempts");
+    } else
+        Serial.println("ERROR: max_attempt reached to MQTT connect");
+    
+    return attempt < max_attempt;
+}
+
+bool read_sensors(float &t, float &h){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+
+  if (isnan(h) || isnan(t)) {
+      delay(2000);
+      h = 0.0; t = 0.0; 
+      return false;
+  } else {
+      // adjust DHT11
+      h = humid_slope * h + humid_shift;
+      t = temp_slope * t + temp_shift; // Read temperature as C
+      return true;
+  }
+}
+
+bool setup_wifi() {
+
+    // init the WiFi connection
+    Serial.println();
+    Serial.print("INFO: Connecting to ");
+    Serial.println(WIFI_SSID);
+    WiFi.mode(WIFI_STA);
+    
+    //  Enable light sleep
+    wifi_set_sleep_type(LIGHT_SLEEP_T);
+    
+    if (local_IP != IPAddress(0,0,0,0) && !WiFi.config(local_IP, gateway, subnet)){
+        Serial.println("STA Failed to configure fixed IP");
+        return false;    
+    }
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    
+    static const int max_attempt = 100;
+    int attempt = 0;
+    while (attempt < max_attempt && WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        attempt++;
+    }
+    Serial.println("");
+
+    return (attempt < max_attempt);
+}
+
+void setup_ac() {
+    ac.begin();
+    logger_info("Setting initial state for A/C.");
+    ac.setFan(kSamsungAcFanLow);
+    ac.setMode(kSamsungAcFan);
+    ac.setTemp(25);
+    ac.setSwing(false);
+    ac_turn_off();
+}
+
+int last_read_time;
+
+void setup() {
+    // init the serial
+    Serial.begin(115200);
+    //Take some time to open up the Serial Monitor
+    delay(1000);
+    
+    dht.begin();
+    
+    // Restart ESP if max attempt reached
+    if (!setup_wifi()){
+        Serial.println("ERROR: max_attempt reached to WiFi connect");
+        Serial.print("Waiting and Restaring");
+        WiFi.disconnect();
+        delay(1000);
+        ESP.restart();
+    }
+
+    // init the MQTT connection
+    client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+    client.setCallback(callback);
+    mqtt_connect();
+
+    setup_ac();
+
+    last_read_time = 0;
+}
+
+int n_sensor_reads = 0;
+float ac_t = 0.0, ac_h = 0.0;
+
+void loop() {
+    float t, h;
+
+    // Serial.println("Inside loop");
+    if (client.connected() || mqtt_connect()) {
+        client.loop();
+        
+        // Average reads
+        if (read_sensors(t, h)){
+            //Serial.print("read temp: "); Serial.print(t);
+            //Serial.print("read humid: "); Serial.println(h);            
+            n_sensor_reads++;
+            ac_t += t;
+            ac_h += h;
+        }
+
+        if (last_read_time >= READ_SENSOR_TIME_IN_SECONDS*1000){
+            if (n_sensor_reads != 0)
+                // Average reads
+                publish_data_sensor(ac_t/n_sensor_reads, ac_h/n_sensor_reads);
+            else
+                logger_warn("Failed to read from DHT sensor!");
+
+            ac_t=0.0; ac_h=0.0;
+            n_sensor_reads = 0;
+
+            print_ac_state();
+            publish_ac_state();
+            last_read_time = 0;
+        } 
+    }
+
+    last_read_time += SLEEPING_TIME_IN_MSECONDS;
+    delay(SLEEPING_TIME_IN_MSECONDS);
+}
