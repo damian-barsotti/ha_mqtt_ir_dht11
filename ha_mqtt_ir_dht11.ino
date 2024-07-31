012ba7f5ca6ab8cd99ae62274e02fef83f45252f
BuildTools <damian@famaf.unc.edu.ar>
2024-07-31 00:01:12 -0300

    Announce callback called

--- ha_mqtt_ir_dht11.ino
+++ ha_mqtt_ir_dht11.ino
@@ -234,6 +234,8 @@ void callback(char *topic, byte *payload, unsigned int length)
 {
     char command[length + 1];
 
+    mqtt.callback_called();
+
     if (strcmp(topic, MQTT_AC_GET_TOPIC) == 0 ||
         strcmp(topic, MQTT_LOG_TOPIC) == 0 ||
         strcmp(topic, MQTT_SENSOR_TOPIC) == 0)
